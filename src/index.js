9983104f4140b19b5ad6b848e085404d91f833f2
Soner Mezgitci <you@example.com>
2020-01-15 11:11:32 -0500

    soner

--- src/index.js
+++ src/index.js
@@ -1,1 +1,49 @@
-const theatreId = null;
+const theatre=  51
+const theaterList=  document.querySelector("#theatreId")
+let movieName = document.getElementById('ui cards showings')
+let movieContent = document.getElementById('content')
+
+fetch("https://evening-plateau-54365.herokuapp.com/theatres/51")
+.then(r => r.json())
+.then ((movieArr) => {
+    movieArr.forEach(movieObj => {
+        TakeOneMovieToLi(movieObj)
+        
+    })
+})
+
+function TakeOneMOvieToLi(movieObj){
+    let movieNameLi = document.createElement("li")
+    movieNameLi.className = "card"
+    movieNameLi.innerText = movieObj.name
+
+}
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    // Obj.keys(Obj).forEach(key => {
+    
+    //     console.log(`key: ${key}, value: ${Obj[key]}`)
+   
+        
+    // if (typeof obj[key] === 'ticketObject') {
+    //             iterate(obj[key])
+    //         }
+    //     })
+    // })
+
+
+
+
+
+
+
+
